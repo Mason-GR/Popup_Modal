05ae95f6f2e607c671bbf77aaee096a19686114e
Mason-GR <mase210@gmail.com>
2021-12-06 21:49:58 -0500

    finished script

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ for (const el of openEls) {
     document.getElementById(modalId).classList.add(isVisible);
   });
 }
+
+for (const el of closeEls) {
+  el.addEventListener("click", function () {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
